168d71faf7118e23be79c156f423be242a7943d9
Boris Syrpin <betelgeusexru@gmail.com>
2023-06-18 23:25:43 +0500

    basic implementation of event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:9944/events", event);
+  axios.post("http://localhost:9945/events", event);
+  axios.post("http://localhost:9946/events", event);
+
+  res.send({ status: "OK" });
+});
+
+app.listen(9949, () => {
+  console.log("Listening on 9949");
+});
