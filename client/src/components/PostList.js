35ee34c79850eab81a6d0c4d96a0afc585eee5de
betelgeusexru <betelgeusexru@gmail.com>
2023-06-18 21:01:18 +0500

    and another one change

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { CommentCreate } from "./CommentCreate"
+import { CommentList } from "./CommentList"
 
 export const PostList = () => {
     const [posts, setPosts] = useState([])
@@ -21,6 +22,7 @@ export const PostList = () => {
                     <h3>
                         {post.title}
                     </h3>
+                    <CommentList postId={post.id} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
