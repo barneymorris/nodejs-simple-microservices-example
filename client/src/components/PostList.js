09d8cfcc3282717893df96dabaf9d2af5ebc75a2
betelgeusexru <betelgeusexru@gmail.com>
2023-06-21 01:28:08 +0500

    using the query service

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -7,7 +7,7 @@ export const PostList = () => {
   const [posts, setPosts] = useState([]);
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:9944/posts");
+    const res = await axios.get("http://localhost:9947/posts");
     setPosts(res.data);
   };
 
@@ -24,7 +24,7 @@ export const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
--- query/index.js
+++ query/index.js
@@ -6,9 +6,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/post", (req, res) => {});
+const posts = {};
 
-app.post("/events", (req, res) => {});
+app.get("/post", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { it, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(9947, () => {
   console.log("Listening on 9947");
